37e9321527cbb2ceb046e36ac0343e7115141e5f
Daniel Hu <shady030314@gmail.com>
2023-04-27 08:16:07 +0800

    add un-pushed file

---
+++ include/queue/sqqueue.h
@@ -0,0 +1,23 @@
+#ifndef SQQUEUE_H_
+#define SQQUEUE_H_
+
+#include <forehead/include.h>
+#define QUEUEMAXSIZE 5
+#define ElemType int
+
+typedef struct {
+  ElemType data[QUEUEMAXSIZE];  // static allocation
+  int front;
+  int rear;
+} SqQueue;
+
+SqQueue *InitSqQueue();
+STATUS DestroySqQueue(SqQueue *Q);
+STATUS ClearSqQueue(SqQueue *Q);
+bool SqQueueEmpty(SqQueue *Q);
+int SqQueueLength(SqQueue *Q);
+void printSqQueue(SqQueue *Q);
+STATUS EnSqQueue(SqQueue *Q, int e);
+STATUS DeSqQueue(SqQueue *Q, int *e);
+
+#endif
